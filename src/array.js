cd7b8ac16de7696f3b144d41a58eca8b1af4ca69
SonderzzZ <hewentaowx@gmail.com>
2019-01-23 10:30:27 +0800

    [update]: 更新array文件

--- src/array.js
+++ src/array.js
@@ -1,17 +1,3 @@
-// 很多初学者总会陷入过分追求性能的误区，而忽略了语义和可读性。
-// 如果只谈性能，显然是 for > forEach > map，
-// 为什么 for 的性能比较好？读一下 forEach 和 map 的规范就知道了。
-// map 会返回一个等长数组，forEach 不会，所以 forEach 大于 map。
-// 但是哪个快和应该使用哪个，并不应该划等号。
-// 如果你需要将数组按照某种规则映射为另一个数组，就应该用 map。
-// 如果你需要进行简单的遍历，用 forEach 或者 for of。
-// 如果你需要对迭代器进行遍历，用 for of.
-// 如果你需要过滤出符合条件的项，用 filter.
-// 如果你需要先按照规则映射为新数组，再根据条件过滤，那就用一个 map 加一个 filter。
-// 不要担心这样会慢，你那点数据量浏览器根本不 care。
-// 如果你真的需要考虑性能，或者有 break 的需求，就用 for 吧。
-// 但是如果真的到了这一步，你应该不会来问这个问题。
-
 /**
  * map()方法返回一个新数组，结果为该数组中的每一个元素都调用函数后返回的结果。
  *
@@ -110,6 +96,7 @@ console.log(copy); // [1, 2, 3, 4, 5]
  * array(可选)          调用reduce()的数组
  * initialValue(可选)   第一次调用callback函数的第一个参数值
  */
+
 // 返回累积值
 const arr4 = [1, 2, 3, 4, 5];
 const arr4_result = arr4.reduce((a, c) => {
@@ -123,13 +110,66 @@ const arr5_result = arr5.reduce((a, c) => {
 }, 10);
 console.log(arr5_result); // 25
 
-/**
- * for...in
- */
+// 取出对象中的 name 单独放在一个数组中
+const arr6 = [
+  {id: 10, name: 'zhangsan'},
+  {id: 11, name: 'lisi'},
+  {id: 12, name: 'wangwu'},
+];
+const arr6_result = arr6.reduce((a, c) => {
+  return [
+    ...a,
+    c.name
+  ];
+}, []);
+console.log(arr6_result); // [ 'zhangsan', 'lisi', 'wangwu' ]
 
 /**
- * for...of
+ * for...in任意顺序遍历一个对象的可枚举属性。对于每个不同的属性，语句都会被执行。
+ * 但是只遍历可枚举的属性，需要注意的是枚举的时候还会枚举到对象从其构造函数原型中继承的属性
+ *
+ * 语法：
+ * for (variable in object) {...}
+ *
+ * 参数说明：
+ * variable 属性名
+ * object   被迭代枚举其属性的对象
  */
+
+// 遍历key
+const obj = {
+  name: 'sonderzzz',
+  age: 26,
+  sex: 'male',
+};
+for (let v in obj) {
+  console.log(`${v} is ${obj[v]}`);
+}
+
+// 注意：有原型方法时，for...in会遍历到原型链上的属性
+const arr7 = [1, 2, 3, 4, 5];
+Array.prototype.len = function() {
+  return this.length;
+};
+arr7.len();
+for (let v in arr7) {
+  console.log(v); // 0 1 2 3 4 len
+}
+
+/**
+ * for...of一个数据结构只要部署了Symbol.iterator属性，就被视为具有 iterator 接口，就可以用for...of循环遍历它的成员。
+ * for...of读取的是键值 for...in读取的是键名
+ * 语法：
+ * for (variable of object) {...}
+ *
+ * 参数说明：
+ * variable 属性名
+ * object   被迭代枚举其属性的对象
+ */
+const arr8 = ['red', 'green', 'blue'];
+for (let v of arr8) {
+  console.log(v);
+}
 
 /**
  * some()方法用于检测数组中的元素是否满足指定条件，依次执行数组中的每个元素，一旦有元素满足则后续不再执行返回true。
@@ -147,11 +187,11 @@ console.log(arr5_result); // 25
  */
 
 // 判断数组中是否有大于5的元素
-const arr6 = [1, 2, 3, 4, 5];
-const arr6_result = arr6.some(c => {
+const arr10 = [1, 2, 3, 4, 5];
+const arr10_result = arr10.some(c => {
   return c > 5;
 });
-console.log(arr6_result); // false
+console.log(arr10_result); // false
 
 /**
  * every()方法用于检测数组中每一个元素是否满足指定条件，有一个元素不满足则后续不再执行返回false。
@@ -169,8 +209,17 @@ console.log(arr6_result); // false
  */
 
 // 判断数组中是否每个元素都小于5
-const arr7 = [1, 2, 3, 4, 5];
-const arr7_result = arr7.every(c => {
+const arr11 = [1, 2, 3, 4, 5];
+const arr11_result = arr11.every(c => {
   return c < 5;
 });
-console.log(arr7_result); // false
+console.log(arr11_result); // false
+
+
+// 总结：
+// 如果需要将数组按照某种规则映射为另一个数组应该用 map。
+// 如果需要进行简单的遍历，用 forEach 或者 for...of。
+// 如果需要对迭代器进行遍历，用 for...of.
+// 如果需要过滤出符合条件的项，用 filter.
+// 如果需要先按照规则映射为新数组，再根据条件过滤，那就用一个 map 加一个 filter。
+// 最后慎用 for...in
